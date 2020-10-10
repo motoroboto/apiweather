2fe9ddd51ec893dff15f645c094d7fe628110b1e
motoroboto <robert.a.cullen@gmail.com>
2020-10-10 01:57:09 -0400

    working on the script, pusing before I go to bed just to be safe

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,40 @@
 function displayCityWeather(f) {
     f.preventDefault();
     var city = $('.cityInput').val().trim();
-    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&apikey=f82f3320f3be5227d4c1e1a02d86687b';
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=f82f3320f3be5227d4c1e1a02d86687b';
   
 
     $.ajax({
       url: queryURL,
       method: 'GET',
-    }).then(function (response) {
-      console.log('response:', response)
+    }).then(function (responseWeather) {
+      console.log('response:', responseWeather)
+      console.log('current weather')
     });
       
 
 
 };
 
+// api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}
+
+function displayCityForecast(f2) {
+  f2.preventDefault();
+  var city = $('.cityInput').val().trim();
+  var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=f82f3320f3be5227d4c1e1a02d86687b';
+
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET',
+  }).then(function (responseForecast) {
+    console.log('response:', responseForecast)
+    console.log('forecast')
+  });
+    
+
+
+};
+
 $(document).on('click', '.searchBtn', displayCityWeather);
+$(document).on('click', '.searchBtn', displayCityForecast);
